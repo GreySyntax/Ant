ab275e2a6c5c8505a9dae53323aabe417bc81704
Jordan M <jordan@meepmail.co.uk>
2010-07-22 12:10:03 +0100

    The DatabaseManager now creates the Minimum connections when StartManager is called. If we recive >5 errors during the connect sequence we call StopManager and reutrn false.

--- AntLibrary/Storage/DatabaseManager.cs
+++ AntLibrary/Storage/DatabaseManager.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data;
+
+using MySql.Data.MySqlClient;
 
 using AntLibrary.Util;
 
@@ -16,7 +20,28 @@ namespace AntLibrary.Storage
 
         private string _name;
 
-        public DatabaseManager(string server, uint port, string user, string password, string name)
+        private uint _min;
+        private uint _max;
+
+        private MySqlConnectionStringBuilder _connectionString;
+
+        private Dictionary<uint, DatabaseClient> _clients;
+
+        private List<uint> _busy;
+        private List<uint> _temporary;
+
+        private uint _nextID = 1;
+        private object _wait;
+
+        public string ConnectionString
+        {
+            get
+            {
+                return _connectionString.ToString();
+            }
+        }
+
+        public DatabaseManager(string server, uint port, string user, string password, string name, uint min, uint max)
         {
             _server = server;
             _port = port;
@@ -25,6 +50,9 @@ namespace AntLibrary.Storage
             _password = password;
 
             _name = name;
+
+            _min = min;
+            _max = max;
         }
 
         public bool GetRunning()
@@ -47,17 +75,101 @@ namespace AntLibrary.Storage
 
         public bool StartManager()
         {
+            if (_server.Length == 0 && _user.Length == 0 && _password.Length == 0 && _name.Length == 0)
+            {
+                Logging.LogEvent("DatabaseManager", "Invalid credentials (server/user/password/name).", Logging.ELogLevel.ERROR);
+                return false;
+            }
 
+            _wait = new object();
+            _connectionString = new MySqlConnectionStringBuilder();
+
+            _connectionString.Server = _server;
+            _connectionString.Port = _port;
+
+            _connectionString.UserID = _user;
+            _connectionString.Password = _password;
+            _connectionString.Database = _name;
+
+            _connectionString.MinimumPoolSize = _min;
+            _connectionString.MaximumPoolSize = _max;
+
+            _clients = new Dictionary<uint, DatabaseClient>();
+            _busy = new List<uint>();
+            _temporary = new List<uint>();
+
+            uint e = 0;
+
+            while (_min >= _clients.Count && 5 >= e)
+            {
+                uint id = Connect(false);
+
+                if (id == 0)
+                    e++;
+            }
+
+            if (e >= 5)
+            {
+                SetRunning(false);
+                return false;
+            }
 
             SetRunning(true);
             return true;
         }
 
-        private void StopManager()
+        public void StopManager()
         {
 
             if (_running)
                 SetRunning(false, true);
+        }
+
+        public uint Connect(bool temporary)
+        {
+            uint id = 0;
+
+            try
+            {
+                MySqlConnection connection = new MySqlConnection(ConnectionString);
+                connection.Open();
+
+                uint tries = 10;
+                while (connection.State == ConnectionState.Connecting && tries > 0)
+                {
+                    //Lets hope the state changes!
+                    tries--;
+                }
+
+                if (0 >= tries)
+                {
+                    connection.Close();
+                    return 0;
+                }
+
+                lock (_wait)
+                {
+                    id = _nextID;
+
+                    _clients.Add(id, new DatabaseClient(id, connection));
+
+                    if (temporary)
+                        _temporary.Add(id);
+
+                    _nextID++;
+                }
+            }
+            catch (MySqlException e)
+            {
+                Logging.LogEvent("DatabaseServer", string.Format("Failed to create DatabaseClient with error: {0}", e.Message), Logging.ELogLevel.WARNING);
+
+                if (id > 0)
+                {
+                    //Remove
+                }
+            }
+
+            return id;
         }
     }
 }
