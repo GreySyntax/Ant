5368ca4f38fdb5192cd519277010b8a92ec56139
Jordan M <jordan@meepmail.co.uk>
2010-07-22 14:23:51 +0100

    Implemented SHA512 hashing for passwords etc.

---
+++ AntLibrary/Util/Security.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace AntLibrary.Util
+{
+    class Security
+    {
+        public static string CalculateSHA512(string text)
+        {
+            byte[] buffer = Encoding.Default.GetBytes(text);
+            SHA512CryptoServiceProvider cryptoTransformSHA1 =
+            new SHA512CryptoServiceProvider();
+            string hash = BitConverter.ToString(
+                cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+
+            return hash;
+        }
+    }
+}
