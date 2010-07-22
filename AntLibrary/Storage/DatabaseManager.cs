76343dfb4b7a5b312ed2c17b9dfdf9564b88b63f
Jordan M <jordan@meepmail.co.uk>
2010-07-22 11:44:41 +0100

    Started on Manager interface and DatabaseManager

--- AntLibrary/Storage/DatabaseManager.cs
+++ AntLibrary/Storage/DatabaseManager.cs
@@ -1,11 +1,63 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using AntLibrary.Util;
 
 namespace AntLibrary.Storage
 {
-    class DatabaseManager
+    class DatabaseManager : IManager
     {
+        private bool _running;
+
+        private string _server;
+        private uint _port;
+
+        private string _user;
+        private string _password;
+
+        private string _name;
+
+        public DatabaseManager(string server, uint port, string user, string password, string name)
+        {
+            _server = server;
+            _port = port;
+
+            _user = user;
+            _password = password;
+
+            _name = name;
+        }
+
+        public bool GetRunning()
+        {
+            return _running;
+        }
+
+        public void SetRunning(bool value)
+        {
+            SetRunning(value, false);
+        }
+
+        public void SetRunning(bool value, bool soft)
+        {
+            _running = value;
+
+            if (!_running && !soft)
+                StopManager();
+        }
+
+        public bool StartManager()
+        {
+
+
+            SetRunning(true);
+            return true;
+        }
+
+        private void StopManager()
+        {
+
+            if (_running)
+                SetRunning(false, true);
+        }
     }
 }
---
+++ AntLibrary/Util/IManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntLibrary.Util
+{
+    interface IManager
+    {
+        bool StartManager();
+        void StopManager();
+
+        bool GetRunning();
+        void SetRunning(bool value);
+        void SetRunning(bool value, bool soft);
+    }
+}
