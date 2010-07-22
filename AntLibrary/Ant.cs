070200c3cf22581f8515f96e7d17b6857890d500
Jordan M <jordan@meepmail.co.uk>
2010-07-22 11:04:07 +0100

    Initial codebase.

---
+++ AntLibrary/Ant.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Threading;
+
+using AntLibrary.Util;
+
+namespace AntLibrary
+{
+    public class Ant
+    {
+        //Instance
+        private static Ant _ant;
+
+        //Objects
+        private DateTime _started;
+        private bool _running = true;
+        
+        //Managers
+        public static Ant Get()
+        {
+            if (_ant == null)
+                _ant = new Ant();
+            return _ant;
+        }
+
+        //Getters & Setters
+        public bool IsRunning
+        {
+            get
+            {
+                return _running;
+            }
+            set
+            {
+                _running = value;
+
+                if (!_running)
+                {
+                    //Shutdown the application
+                    DateTime shutdown = DateTime.Now;
+
+                    //Debug information
+
+                    Thread.Sleep(1000);
+                    Environment.Exit(1);
+                }
+            }
+        }
+
+        //Methods
+        public void StartAnt()
+        {
+            Console.Title = "Ant IRCd";
+            Console.CursorVisible = false;
+
+            Console.WindowHeight = 500;
+            Console.WindowWidth = 100;
+
+            Logging.WriteRaw("");
+            Logging.WriteRaw("***");
+            Logging.WriteRaw("* Ant IRCd");
+            Logging.WriteRaw("* <puppet> a woman can fake an orgasm, but it takes a man to fake an entire relationship");
+            Logging.WriteRaw("**");
+            Logging.WriteRaw("");
+
+            _started = DateTime.Now;
+            Logging.LogEvent("AntCore", string.Format("Startup time {0}", _started.ToString()), Logging.ELogLevel.INFO);
+
+            Thread.CurrentThread.Name = "AppThread";
+            Logging.LogEvent("AntCore", "Created AppThread", Logging.ELogLevel.INFO);
+        }
+    }
+}
---
+++ AntLibrary/Util/Logging.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AntLibrary.Util
+{
+    class Logging
+    {
+        private static object _writeLock = new object();
+
+        public enum ELogLevel
+        {
+            DEBUG,
+            INFO,
+            WARNING,
+            ERROR
+        }
+
+        public static void WriteRaw(string message)
+        {
+            lock (_writeLock)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine(message);
+            }
+        }
+
+        public static void LogEvent(string system, string message, ELogLevel level)
+        {
+            message = message.Replace("\r", "\r\n");
+
+            lock (_writeLock)
+            {
+                //[TIME] [SYSTEM] MESSAGE
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("[{0}] [", DateTime.Now.ToString());
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(system);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("] ");
+
+                switch (level)
+                {
+                    case ELogLevel.DEBUG:
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        break;
+                    case ELogLevel.INFO:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+                    case ELogLevel.WARNING:
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        break;
+                    case ELogLevel.ERROR:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        break;
+                }
+
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
